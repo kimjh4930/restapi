98ca411a9884570a185c1ede4dc0cd5cfbe1ada3
Junha Kim <kimjh4930@gmail.com>
2021-01-09 15:48:09 +0900

    Event domain

---
+++ src/main/java/com/example/restapi/events/Event.java
@@ -0,0 +1,26 @@
+package com.example.restapi.events;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location;
+    private int basePrice;
+    private int maxPrice;
+    private int limitOfEnrollment;
+    private boolean offline;
+    private boolean free;
+    private EventStatus eventStatus = EventStatus.DRAFT;
+}
